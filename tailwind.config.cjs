bad6d6dde7ffcba6b1c0f24b61275cf69d8230de
Ryan LaBouve <imryanlabouve@gmail.com>
2021-08-12 11:05:04 -0500

    Even better

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,11 @@
+const production = !process.env.ROLLUP_WATCH;
+
 module.exports = {
   purge: {
     content: [
       "./src/**/*.svelte",
     ],
-    enabled: "production" // disable purge in dev
+    enabled: production // disable purge in dev
   },
   darkMode: "media", // false or 'media' or 'class':: https://tailwindcss.com/docs/dark-mode
   theme: {
