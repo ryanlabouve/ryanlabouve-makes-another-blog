3fd00df9ef5202229b56a47bde88bc154f15df56
Ryan LaBouve <imryanlabouve@gmail.com>
2021-08-12 00:07:44 -0500

    Installs tailwind

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,10 @@
+<script>
+  import './tailwind.css';
+</script>
+
+<main class="w-full h-screen 
+  text-gray-800 dark:text-gray-100
+  bg-gray-100 dark:bg-gray-800
+">
+  <slot />
+</main>
---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/**/*.{js,ts,jsx,tsx,.svelte}'],
+  darkMode: "media", // false or 'media' or 'class':: https://tailwindcss.com/docs/dark-mode
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
